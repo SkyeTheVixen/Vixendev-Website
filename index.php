17da82bd5ebcc5b290571f4309e3b7e5da7f7228
Skylar <Skylar.beacham@outlook.com>
2021-11-04 21:21:05 +0000

    Added scripts to header and footer

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php $title="Home"; ?>
 <?php $currentPage="index"; ?>
+<?php include("res/php/header.php"); ?>
+
+
+<!-- Main Page Content -->
+<h1>Welcome</h1>
+<!-- End Main Page Content -->
+
+
+<?php include("res/php/footer.php"); ?>

4ae08402b6cc5b07479609babb433da0774ac884
Skylar <skylar@vixendev.com>
2022-06-29 16:42:44 +0100

    socials update

--- socials.php
+++ socials.php
@@ -5,27 +5,44 @@
 
 <html lang="en">
 
-    <head>
-        <title><?=$title?></title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.css">
-    </head>
+<head>
+    <title><?=$title?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.css">
+</head>
 
-    <body class="bg-dark">
+<body class="bg-dark">
     <div clas="row vh-100">
         <div class="col-12 vh-100 text-center">
             <h1 class="text-light mt-5">Welcome to Skye's Socials</h1>
-                <a class="btn btn-primary mx-5">Discord: Skye The Vixen#7084</a>
-                <a class="btn btn-primary mx-5" href="https://instagram.com/skyethevixen">Instagram</a>
-                <a class="btn btn-primary mx-5" href="https://twitter.com/skyethevixen">Twitter</a>
-                <a class="btn btn-primary mx-5" href="https://twitter.com">AD Twitter</a>
-                <a class="btn btn-primary mx-5" href="https://www.furaffinity.net/user/skyethevixen/">Furaffinity</a>
+        </div>
+    </div>
+    <div clas="row vh-100">
+        <div class="col-12 vh-100 text-center">
+            <a class="btn btn-primary mx-5">Discord: Skye The Vixen#7084</a>
+        </div>
+    </div>
+    <div clas="row vh-100">
+        <div class="col-12 vh-100 text-center">
+            <a class="btn btn-primary mx-5" href="https://twitter.com/skyethevixen">Twitter</a>
+        </div>
+    </div>
+    <div clas="row vh-100">
+        <div class="col-12 vh-100 text-center">
+            <a class="btn btn-primary mx-5" href="https://twitter.com">AD Twitter</a>
+        </div>
+    </div>
+    <div clas="row vh-100">
+        <div class="col-12 vh-100 text-center">
+            <a class="btn btn-primary mx-5" href="https://www.furaffinity.net/user/skyethevixen/">Furaffinity</a>
         </div>
     </div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
